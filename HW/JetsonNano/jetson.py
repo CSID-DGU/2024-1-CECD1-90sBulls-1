350aa746bf14fe07609710f48bd4a83d08196de5
ThatIsBug <86178269+ThatIsBug@users.noreply.github.com>
2024-06-22 16:44:10 +0900

    Update jetson.py

--- HW/JetsonNano/jetson.py
+++ HW/JetsonNano/jetson.py
@@ -9,6 +9,42 @@ import socket
 import base64
 import sys
 
+class ClientSocket:
+    def __init__(self, ip, port):
+        self.TCP_SERVER_IP = ip
+        self.TCP_SERVER_PORT = port
+        self.connectCount = 0
+        self.connectServer()
+
+    def connectServer(self):
+        try:
+            self.sock = socket.socket()
+            self.sock.connect((self.TCP_SERVER_IP, self.TCP_SERVER_PORT))
+            print(
+                f'클라이언트 소켓이 서버 소켓과 연결되었습니다 [ TCP_SERVER_IP: {self.TCP_SERVER_IP}, TCP_SERVER_PORT: {self.TCP_SERVER_PORT} ]')
+            self.connectCount = 0
+        except Exception as e:
+            print(e)
+            self.connectCount += 1
+            if self.connectCount == 10:
+                print(f'{self.connectCount}회 연결 실패. 프로그램 종료')
+                sys.exit()
+            print(f'{self.connectCount}회 서버와 연결 시도 중')
+            time.sleep(5)
+            self.connectServer()
+
+
+		def sendImages(self, image_path):
+		    image = cv2.imread(image_path)
+		    encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
+		    result, imgencode = cv2.imencode('.jpg', image, encode_param)
+		    data = np.array(imgencode)
+		    stringData = base64.b64encode(data)
+		    length = str(len(stringData))
+		    self.sock.sendall(length.encode('utf-8').ljust(64))
+		    self.sock.send(stringData)
+		    print("전송 완료")
+
 class Camera:
     def __init__(self):
         self.camera = jetson.utils.videoSource("csi://0")  # CSI 카메라 초기화
