02cc40f1d361736b241f59b227085957e065e89f
changhyun9 <ckdgus1713@naver.com>
2024-06-24 17:22:20 +0900

    Modify server.py - Separate function

--- SERVER/server.py
+++ SERVER/server.py
@@ -74,3 +74,12 @@ def receiveImages(conn):
     finally:
         clients.remove(conn)
         conn.close()
+
+def recvall(sock, count):
+    buf = b''
+    while count:
+        newbuf = sock.recv(count)
+        if not newbuf: return None
+        buf += newbuf
+        count -= len(newbuf)
+    return buf
