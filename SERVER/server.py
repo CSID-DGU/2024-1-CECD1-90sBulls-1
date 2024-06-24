1431b03d7ff7dfe7f1d5a4737a772442b2486c9a
changhyun9 <ckdgus1713@naver.com>
2024-06-24 17:20:33 +0900

    Add server.py - fucntion of receiveImages

--- SERVER/server.py
+++ SERVER/server.py
@@ -38,3 +38,39 @@ class Socket:
         self.sock.listen()
         print(u'Server socket [ TCP_IP: ' + self.TCP_IP + ', TCP_PORT: ' + str(self.TCP_PORT) + ' ] is open')
 
+
+def receiveImages(conn):
+    try:
+        while True:
+            length = recvall(conn, 64).decode('utf-8')
+            if length is None:
+                break
+            stringData = recvall(conn, int(length))
+            data = numpy.frombuffer(base64.b64decode(stringData), numpy.uint8)
+            decimg = cv2.imdecode(data, 1)
+            cv2.imwrite("saved_image.jpg", decimg)
+
+            results = model.predict("./saved_image.jpg", show=False)
+
+            for result in results:
+                cls = result.boxes.cls
+                if (cls == 0):
+                    value = {"r": 255, "g": 255, "b": 0, "lx": 50, "cls": "Eating person"}
+                elif (cls == 1):
+                    value = {"r": 204, "g": 0, "b": 0, "lx": 50, "cls": "Sleeping person"}
+                elif (cls == 2):
+                    value = {"r": 51, "g": 0, "b": 255, "lx": 50, "cls": "Studying person"}
+                else:
+                    print("No class")
+                    continue
+
+                # change format from dict to json
+                # create new thread for sending JSON data and start
+                json_data = json.dumps(value)
+                send_socket(json_data, conn)
+
+    except Exception as e:
+        print(e)
+    finally:
+        clients.remove(conn)
+        conn.close()
