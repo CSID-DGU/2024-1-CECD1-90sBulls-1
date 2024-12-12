19c65ebf1e04121cf32427884522475e5bf64cab
JIHO <45422452+kimjiho1125@users.noreply.github.com>
2024-12-12 15:42:11 +0900

    feat: 이미지 전송 기능 추가
    1. send_images 메서드 작성.
    2. JPEG 인코딩 및 Base64 전송 로직 구현.

--- HW/JetsonNano/jetson.py
+++ HW/JetsonNano/jetson.py
@@ -33,3 +33,14 @@ class ClientSocket:
             print(f'{self.connect_count}회 서버와 연결 시도 중')
             time.sleep(5)
             self.connect_server()
+
+    def send_images(self, image_path):
+    image = cv2.imread(image_path)
+    encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]  # JPEG 품질 설정
+    result, imgencode = cv2.imencode('.jpg', image, encode_param)
+    data = np.array(imgencode)
+    string_data = base64.b64encode(data)
+    length = str(len(string_data))
+    self.sock.sendall(length.encode('utf-8').ljust(64))
+    self.sock.send(string_data)
+    print("이미지 전송 완료")
