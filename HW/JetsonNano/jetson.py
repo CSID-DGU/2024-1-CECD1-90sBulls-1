b8c203ffc967a6229753d167cfd2985bd647d1d2
JIHO <45422452+kimjiho1125@users.noreply.github.com>
2024-12-12 15:37:50 +0900

    Create jetson.py and add initial imports and basic setup
    Create jetson.py and add initial imports and basic setup

---
+++ HW/JetsonNano/jetson.py
@@ -0,0 +1,12 @@
+from ultralytics import YOLO
+import RPi.GPIO as GPIO
+import time
+import cv2
+import os
+import numpy as np
+import socket
+import base64
+import sys
+import threading
+import torch
+import gc
