c13aad6dc2db2810e42b6135f9e75d7e76b15e15
changhyun9 <ckdgus1713@naver.com>
2024-12-12 23:33:06 +0900

    Feat setting server

--- SERVER/server.py
+++ SERVER/server.py
@@ -18,3 +18,10 @@ cyclegan_model.eval()
 cyclegan_model.unet.enable_xformers_memory_efficient_attention()
 
 model = YOLO('./best.pt')
+
+# 클라이언트 연결을 위한 큐
+client_queue = queue.Queue()
+
+SERVER_IP = '175.45.194.40'
+SERVER_PORT = 8080
+TIMEOUT = 10  # 소켓 타임아웃 설정 (10초)
