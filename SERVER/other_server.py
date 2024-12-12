3af8348029894c6f2aade8026c46d8f15708bea8
changhyun9 <ckdgus1713@naver.com>
2024-12-12 23:58:20 +0900

    Feat Socket class

--- SERVER/other_server.py
+++ SERVER/other_server.py
@@ -24,3 +24,22 @@ clients = []
 
 SERVER_IP = '175.45.194.40'
 SERVER_PORT = 8080
+
+class Socket:
+
+    def __init__(self, ip, port):
+        self.TCP_IP = ip
+        self.TCP_PORT = port
+        self.sock = None
+        self.socketOpen()
+
+    def socketClose(self):
+        self.sock.close()
+        print(u'Server socket [ TCP_IP: ' + self.TCP_IP + ', TCP_PORT: ' + str(self.TCP_PORT) + ' ] is close')
+
+    def socketOpen(self):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+        self.sock.bind((self.TCP_IP, self.TCP_PORT))
+        self.sock.listen()
+        print(u'Server socket [ TCP_IP: ' + self.TCP_IP + ', TCP_PORT: ' + str(self.TCP_PORT) + ' ] is open')
