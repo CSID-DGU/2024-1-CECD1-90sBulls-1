73cd630d1c3a3f0a41431c7f160887193916a215
ThatIsBug <86178269+ThatIsBug@users.noreply.github.com>
2024-12-12 16:28:15 +0900

    feat: MQTT관련 설정 변수 정의
    MQTT관련 설정 변수 정의

--- HW/RaspberryPi/raspberry.py
+++ HW/RaspberryPi/raspberry.py
@@ -9,3 +9,9 @@ import paho.mqtt.client as mqtt
 
 SERVER_IP = '211.188.63.60'
 SERVER_PORT = 8080
+
+BROKER = "localhost"
+PORT = 1883
+TOPIC_SUBSCRIBE_LIGHT = "app/light"
+TOPIC_SUBSCRIBE_SCENARIO = "app/scenario"
+TOPIC_PUBLISH = "pi/light"
