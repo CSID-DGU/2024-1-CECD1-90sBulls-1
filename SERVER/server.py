29364f43b747b26c7cd51e8d473d7328f225d7e2
changhyun9 <ckdgus1713@naver.com>
2024-12-12 23:31:51 +0900

    Add load model code

--- SERVER/server.py
+++ SERVER/server.py
@@ -12,4 +12,9 @@ import threading
 from datetime import datetime
 from ultralytics import YOLO
 
+# CycleGAN 및 YOLO 모델 로드
+cyclegan_model = CycleGAN_Turbo(pretrained_name="night_to_day")
+cyclegan_model.eval()
+cyclegan_model.unet.enable_xformers_memory_efficient_attention()
 
+model = YOLO('./best.pt')
