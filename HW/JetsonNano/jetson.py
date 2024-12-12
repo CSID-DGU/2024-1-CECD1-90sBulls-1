f75b8eca7e131ca0f0681e06065c1e82dc2a7a15
JIHO <45422452+kimjiho1125@users.noreply.github.com>
2024-12-12 15:40:35 +0900

    feat: add ClientSocket class with server connection functionality
    feat: add ClientSocket class with server connection functionality

--- HW/JetsonNano/jetson.py
+++ HW/JetsonNano/jetson.py
@@ -10,3 +10,26 @@ import sys
 import threading
 import torch
 import gc
+
+class ClientSocket:
+    def __init__(self, ip, port):
+        self.TCP_SERVER_IP = ip
+        self.TCP_SERVER_PORT = port
+        self.connect_count = 0
+        self.connect_server()
+
+    def connect_server(self):
+        try:
+            self.sock = socket.socket()
+            self.sock.connect((self.TCP_SERVER_IP, self.TCP_SERVER_PORT))
+            print(f'클라이언트 소켓이 서버 소켓과 연결되었습니다 [ IP: {self.TCP_SERVER_IP}, PORT: {self.TCP_SERVER_PORT} ]')
+            self.connect_count = 0
+        except Exception as e:
+            print(e)
+            self.connect_count += 1
+            if self.connect_count == 10:
+                print(f'{self.connect_count}회 연결 실패. 프로그램 종료')
+                sys.exit()
+            print(f'{self.connect_count}회 서버와 연결 시도 중')
+            time.sleep(5)
+            self.connect_server()
